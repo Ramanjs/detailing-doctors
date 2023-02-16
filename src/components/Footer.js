d2d7130f6578a7b58064842cd3401808ee494a22
Ramanjs <raman.s2662@gmail.com>
2023-02-16 21:05:20 +0530

    update: footer responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 
 export default function Footer() {
   return (
-    <div className="w-full flex flex-col sm:flex-row justify-between items-center sm:items-start bg-white text-secondary px-2 sm:px-36 py-8">
-      <div className="max-w-[40%]">
+    <div className="w-full flex flex-col sm:flex-row justify-between sm:items-start bg-white text-secondary px-4 sm:px-36 py-8 space-y-4 sm:space-y-0">
+      <div className="w-full sm:max-w-[40%]">
         <p className="text-xl font-bold font-chakra">About Us</p>
         <p>Our products provide world-class protection and value to both new and used automobile buyers. One can find wide range of services which help to the car to retain the same superb look it had when you bought it, and this helps to maintain its value when you sell it.</p>
       </div>
