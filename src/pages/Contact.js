1ae9239a0b0578066a52199f4b2f063c97f9d5c4
Ramanjs <raman.s2662@gmail.com>
2023-02-16 18:21:54 +0530

    add: contact us emailjs

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,48 @@
 import React, {useState} from 'react'
 import { Helmet } from 'react-helmet'
+import emailjs from "@emailjs/browser"
 
 const Contact = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
+  const [phone, setPhone] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
+  const [success, setSuccess] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setLoading(true)
+
+    const templateParams = {
+      name,
+      to_email: email,
+      message,
+      phone,
+    }
+
+    emailjs.send(
+      "service_bsjvemm",
+      "template_91kbnoh",
+      templateParams,
+      "MdQQ8r7SRIsYuy7Vr"
+    )
+      .then(() => {
+        setLoading(false)
+        setSuccess("Thank you for contacting us. We will get back to you shortly.")
+        setTimeout(() => {
+          setSuccess('')
+        }, 5000)
+      })
+      .catch(err => {
+        setLoading(false)
+        setError('Some error occured! Please try again later.')
+        setTimeout(() => {
+          setError('')
+        }, 5000)
+      })
+  }
 
   return (
     <div className="w-full p-4 flex justify-center text-white">
@@ -12,20 +50,32 @@ const Contact = () => {
         <title>Detailing Doctors | Contact Us</title>
         <meta name="description" content="The premium car detailing studio"/>
       </Helmet>
-      <form className="flex flex-col w-1/2">
+      <form className="flex flex-col w-1/2" onSubmit={handleSubmit}>
         <div className="flex flex-col my-4">
           <label className="font-chakra font-semibold uppercase" htmlFor="name">Your name</label>
-          <input className="w-1/2 py-2 border-b-2 border-grey bg-primary text-offwhite placeholder:text-offwhite focus:outline-none focus:border-secondary" id="name" type="text" value={name} placeholder="Enter Your Name" onChange={(e) => setName(e.target.value)}></input>
+          <input className="w-1/2 py-2 border-b-2 border-grey bg-primary text-offwhite placeholder:text-offwhite focus:outline-none focus:border-secondary" id="name" type="text" value={name} placeholder="Enter Your Name" onChange={(e) => setName(e.target.value)} required/>
+        </div>
+        <div className="flex flex-col my-4">
+          <label className="font-chakra font-semibold uppercase" htmlFor="address">Mobile Number</label>
+          <input className="w-1/2 py-2 border-b-2 border-grey bg-primary text-offwhite placeholder:text-offwhite focus:outline-none focus:border-secondary" id="address" type="tel" value={phone} placeholder="Ex: 8918523134" onChange={(e) => setPhone(e.target.value)} required pattern="[0-9]{10}"/>
         </div>
         <div className="flex flex-col my-4">
           <label className="font-chakra font-semibold uppercase" htmlFor="email">Your email</label>
-          <input className="w-1/2 py-2 border-b-2 border-grey bg-primary text-offwhite placeholder:text-offwhite focus:outline-none focus:border-secondary" type="email" value={email} placeholder="Enter Your Email" onChange={(e) => setEmail(e.target.value)}></input>
+          <input className="w-1/2 py-2 border-b-2 border-grey bg-primary text-offwhite placeholder:text-offwhite focus:outline-none focus:border-secondary" id="email" type="email" value={email} placeholder="Enter Your Email" onChange={(e) => setEmail(e.target.value)} required/>
         </div>
         <div className="flex flex-col my-4">
           <label className="font-chakra font-semibold uppercase" htmlFor="message">Your message</label>
-          <input className="py-2 border-b-2 border-grey bg-primary text-offwhite placeholder:text-offwhite focus:outline-none focus:border-secondary" id="message" type="text" value={message} placeholder="Enter your message" onChange={(e) => setMessage(e.target.value)}></input>
+          <input className="py-2 border-b-2 border-grey bg-primary text-offwhite placeholder:text-offwhite focus:outline-none focus:border-secondary" id="message" type="text" value={message} placeholder="Enter your message" onChange={(e) => setMessage(e.target.value)} required/>
         </div>
-        <input className="font-shakra uppercase font-semibold my-4 w-fit py-2 px-4 bg-secondary hover:cursor-pointer" type="submit" value="Send Message"/>
+        {error && <p className="text-red-500">{error}</p>}
+        {success && <p className="text-green-500">{success}</p>}
+        {!loading && <input className="font-chakra uppercase font-semibold my-4 w-fit py-2 px-4 mx-auto bg-secondary hover:cursor-pointer" type="submit" value="Send Message"/>}
+        {loading && (
+          <div className="w-full my-2">
+            <div className="w-8 h-8 mx-auto border-t-4 border-blue-400 rounded-[50%] animate-spin">
+            </div>
+          </div>
+        )}
       </form>
     </div>
   )
