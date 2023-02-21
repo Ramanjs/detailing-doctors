a8cae0b14eb432699248694c69cc2b990ce07cc9
Ramanjs <raman.s2662@gmail.com>
2023-02-21 16:59:30 +0530

    add: media queries to know more

--- src/pages/KnowMore.js
+++ src/pages/KnowMore.js
@@ -1,13 +1,13 @@
 export default function KnowMore() {
   return (
-    <div className="w-full bg-white p-4 border-b-2 border-gray-400">
-      <div className="w-full py-4 px-8 bg-[#EDEDED] flex justify-between items-center rounded-md">
-        <div className="w-[55%] flex flex-col">
+    <div className="w-full bg-white p-4">
+      <div className="w-full py-4 px-8 bg-[#EDEDED] flex flex-col sm:flex-row justify-between items-center rounded-md">
+        <div className="w-full sm:w-[55%] flex flex-col">
           <h2 className="text-4xl text-[#252525]">Get A <b>Tempered Glass</b> For Your Car!</h2>
           <p className="text-xl text-[#777777] my-4">Say goodbye to scratches, harsh weather conditions, and fading paint and transform your car into a head-turner with Patented <b>G.O.T. Diamond Hard Coating</b> by <b>Detailing Doctors.</b> Our team of experts at Detailing Doctors will provide a professional service that will keep your car looking new for years to come.</p>
           <p className="w-full h-72 mt-16 bg-[#D9D9D9] rounded-md"></p>
         </div>
-        <div className="w-[40%] flex flex-col px-8 border-l-2 border-l-[#BBBBBB]">
+        <div className="w-full sm:w-[40%] flex flex-col sm:px-8 mt-8 sm:mt-0 sm:border-l-2 sm:border-l-[#BBBBBB]">
           <h2 className="text-4xl text-[#444444] mb-8">Want to talk to an <b>Expert</b> for Free?</h2>
           <input className="p-2 my-2" type="tel" pattern="[0-9]{10}" placeholder="Your Phone No."/>
           <input className="p-2 my-2" type="text" placeholder="Your Full Name"/>
