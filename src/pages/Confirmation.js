1b3a79ffd61afd1fad382874bf2b4dbe2dec0893
Ramanjs <raman.s2662@gmail.com>
2023-02-26 06:49:47 +0530

    updates

--- src/pages/Confirmation.js
+++ src/pages/Confirmation.js
@@ -9,11 +9,6 @@ export default function Confirmation() {
       <Helmet>
         <title>Detailing Doctors | Contact Us</title>
         <meta name="description" content="The premium car detailing studio"/>
-        <script>
-          window.dataLayer = (window.dataLayer || [])
-          function gtag(){dataLayer.push(arguments)}
-          gtag('event', 'conversion', {'send_to': 'AW-852817015/ooQaCNeZnI4YEPfo05YD'})
-        </script>
       </Helmet>
       <div className="text-white min-h-screen flex flex-col justify-center">
         <h1 className="font-chakra text-5xl font-semibold text-center my-4">{state.heading}</h1>
